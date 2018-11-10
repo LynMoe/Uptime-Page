634c458ebe87d7ae227871c223c4abfb6ee774fa
XiaoLin <i@cgl.li>
2018-11-10 12:58:31 +0800

    新增错误处理

---
+++ config.example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XiaoLin
+ * Date: 2018-11-10
+ * Time: 12:47 PM
+ */
+
+define('UPTIMEROBOT_API_KEY',"u123456-49f48fdsfser3rdfd3b");
+define('SSL_CHECKER',false);
+define('SSL_CHECKER_DATA',"https://api.xxx.xx/ssl/data.json");
+
+define('SITE_NAME',"XiaoLin Services Status");
+define('FOOTER',[
+    'Home' => 'https://cgl.li',
+    'Blog' => 'https://xiaolin.in',
+    'Status' => 'https://xll.li',
+]);
+define('COPYRIGHT',"XiaoLin");
